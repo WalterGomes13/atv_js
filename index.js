3480f6ae0d90f66c8ff5e651167b017f5b186226
WalterGomes13 <167585120+WalterGomes13@users.noreply.github.com>
2024-05-06 18:49:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+let bernardo = [
+  'girafa',
+  'zebra',
+  28,
+  'analfabeto',
+  {
+    nome: "Walter",
+    age:26,
+    isAdm: true
+  },
+  true
+]
+
+console.log(`A quarta linha do seu Array é ${bernardo[4].nome}`)
+
